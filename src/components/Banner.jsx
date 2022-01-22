ed5507cc42185b8e023f4990a596ad568ca7c41a
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-01-23 00:58:41 +0100

    initial commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Banner = () => {
-  return <div></div>;
+  return (
+    <div className="relative">
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showIndicators={false}
+        showStatus={false}
+        showThumbs={false}
+        interval={5000}
+      >
+        <div>
+          <img src="https://links.papareact.com/gi1" alt="" loading="lazy" />
+        </div>
+        <div>
+          <img src="https://links.papareact.com/6ff" alt="" loading="lazy" />
+        </div>
+        <div>
+          <img src="https://links.papareact.com/7ma" alt="" loading="lazy" />
+        </div>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Banner;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
@@ -13,6 +14,8 @@ export default function Home() {
 
     <main className="max-w-7xl mx-auto">
      { /* Banner */ }
+
+     <Banner />
 
 
       {/* Products Container */ }

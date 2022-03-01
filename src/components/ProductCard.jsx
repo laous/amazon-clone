5d2b063ab9f2110d8da7274231f086f60f3a81ec
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-03-01 23:01:38 +0100

    update product card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,12 +3,21 @@ import Currency from "react-currency-formatter";
 import Image from "next/Image";
 
 const ProductCard = ({ title, id, image, price, description, category }) => {
+  const myLoader = ({ src, width, quality }) => {
+    return `${src}`;
+  };
   return (
     <div className="relative flex flex-col m-5 z-30 p-10 bg-white">
       <p className="absolute top-2 right-2 text-xs italic text-gray-400">
         {category}
       </p>
-      <Image src={image} height="200" width="200" objectFit="contain" />
+      <Image
+        loader={myLoader}
+        src={image}
+        height="200"
+        width="200"
+        objectFit="contain"
+      />
       <h4 className="my-3 ">{title}</h4>
       <p className="text-xs my-2 line-clamp-2">{description}</p>
       <div className="mb-5">

a2c7036ab0fa221af6f1264710d3693aaa950de9
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-04-03 21:18:47 +0000

    set up firebase

---
+++ firebase/index.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAJu-FHUWmi710UKbhy9KPlutgRVnsZjKQ",
+    authDomain: "next-9a26e.firebaseapp.com",
+    projectId: "next-9a26e",
+    storageBucket: "next-9a26e.appspot.com",
+    messagingSenderId: "136168654415",
+    appId: "1:136168654415:web:b3c6a899bb16b2c111cc3a"
+  };
